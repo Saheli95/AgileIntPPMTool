6463f44e08103b62ae11bc95e60a70c950c281f2
Saheli <saheli@bnglr.com>
2020-01-06 14:45:42 +0530

    Custom Exception

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -4,14 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
+	
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch(Exception e) {
+			throw new ProjectIdException("Project ID'"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+		}
 		
 	}
 
